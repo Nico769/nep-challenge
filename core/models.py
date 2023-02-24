0a137a0b55588e17b531ea1240c7fa401b4b0136
Nico769 <ncl.lnd769@gmail.com>
2023-02-24 15:23:17 +0100

    add custom user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        RESPONSABILE = "RESPONSABILE"
+        OPERATORE = "OPERATORE"
+
+    role = models.CharField(max_length=50, choices=Role.choices)
