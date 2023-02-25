ada6ed8c806fe042f23cd843957ac255f8f27577
Nico769 <ncl.lnd769@gmail.com>
2023-02-25 22:20:24 +0100

    add `Node` model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 from utils.model_abstracts import UuidModel
 
@@ -19,8 +20,18 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=50, choices=Role.AvailableRoles.choices)
 
 
+class Node(MPTTModel):
+    parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
+    title = models.CharField(max_length=255, blank=False)
+    owner = models.ForeignKey(CustomUser, null=True, on_delete=models.SET_NULL, related_name="owned_nodes")
+
+    class MPTTMeta:
+        order_insertion_by = ["id"]
+
+
 class Resource(UuidModel):
     title = models.CharField(max_length=255, blank=False)
     content = models.TextField()
     owner = models.ForeignKey(CustomUser, null=True, on_delete=models.SET_NULL, related_name="owned_resources")
     accessible_to = models.ManyToManyField(Role)
+    container_node = models.ForeignKey(Node, null=True, on_delete=models.CASCADE, related_name="resources")
