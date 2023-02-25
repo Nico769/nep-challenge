1f89c91002c6cbf1bc25ce91acb5a98b3c7da8ae
Nico769 <ncl.lnd769@gmail.com>
2023-02-25 12:48:53 +0100

    add `Role` and `Resource` models

--- core/models.py
+++ core/models.py
@@ -1,10 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from utils.model_abstracts import UuidModel
 
-class CustomUser(AbstractUser):
-    class Role(models.TextChoices):
+
+class Role(models.Model):
+    class AvailableRoles(models.TextChoices):
         RESPONSABILE = "RESPONSABILE"
         OPERATORE = "OPERATORE"
 
-    role = models.CharField(max_length=50, choices=Role.choices)
+    name = models.CharField(max_length=50, choices=AvailableRoles.choices)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class CustomUser(AbstractUser):
+    role = models.CharField(max_length=50, choices=Role.AvailableRoles.choices)
+
+
+class Resource(UuidModel):
+    title = models.CharField(max_length=255, blank=False)
+    content = models.TextField()
+    owner = models.ForeignKey(CustomUser, null=True, on_delete=models.SET_NULL, related_name="owned_resources")
+    accessible_to = models.ManyToManyField(Role)
